ea7b16f04df6ff36d963f1115bbf334ee31260a5
Jim1111 <jmflint1@gmail.com>
2022-08-02 21:08:35 +0100

    Splash and Instructions screens

--- bowling-game-js.js
+++ bowling-game-js.js
@@ -6,6 +6,8 @@ const ctx = canvas.getContext('2d');
 var w = canvas.width / 2;
 
 var splashSc = true;
+var insSc = false;
+var gameSc = false;
 
 ////////////////////////////////
 /////Translation Menu //////////
@@ -18,24 +20,77 @@ var Tuk = false;
 /////////////////////////////////
 
    
-const title = new Image();
-title.src = "images/bowlingTitle.png";
+const splEnd = new Image();
+splEnd.src = "images/splEnd.png";
+
+const insEnd = new Image();
+insEnd.src = "images/splEnd.png";
+
+const bowlLane = new Image();
+bowlLane.src = "images/bowling-lane.png";
+
+const ball = new Image();
+ball.src = "images/ball.png";
+
+
+
+function SplashEnd(e) {
+       if (ctx.isPointInPath(splEnd.path, e.offsetX, e.offsetY)) {
+           splashSc = false;
+           insSc = true;
+           canvas.removeEventListener("click", SplashEnd);
+        }
+    } 
 
 function Splash() {
-    //ctx.drawImage(title, 0, 0, canvas.width, canvas.height);
     ctx.textAlign = "center";
     ctx.font='900 150px Arial';
     ctx.fillStyle = "purple";
     ctx.fillText("Bowling", w, 220);
-    ctx.font='900 90px Arial';
+    ctx.font='500 90px Comic Sans MS';
     ctx.fillStyle = "silver";
     ctx.fillText("Game", w, 300);
-    ctx.fillStyle = "silver";
-    ctx.globalAlpha = 1.0;
-    ctx.fillRect(0, 450, 715, 235);
-    ctx.textAlign = "center"; 
-    ctx.font = "30px Comic Sans MS";
-    ctx.fillStyle = "navy";
+    ctx.fillRect(0, 450, 715, 235); 
+    ctx.drawImage(splEnd, w/2-40, 465, 450, 200);
+    ctx.font='900 40px Comic Sans MS';
+    ctx.fillStyle = "black";
+    ctx.fillText("Start Game!", w, 575);
+    splEnd.path = new Path2D();
+    splEnd.path.rect(w/2-40, 465, 450, 200);
+    canvas.addEventListener("click", SplashEnd);
+}
+
+function InsEnd(e) {
+    if (ctx.isPointInPath(insEnd.path, e.offsetX, e.offsetY)) {
+        insSc = false;
+        gameSc = true;
+        canvas.removeEventListener("click", InsEnd);
+     }
+ } 
+
+function Ins() {
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.fillRect(50, 80, 616, 550);  
+    ctx.fillStyle = "purple";
+    ctx.font='900 90px Comic Sans MS';
+    ctx.fillText("Bowling Game", w, 180);
+    ctx.font='500 30px Comic Sans MS';
+    ctx.fillText("The game starts with 12 skittles", w, 260);
+    ctx.fillText("You can throw the ball three times", w, 310);
+    ctx.fillText("See how many you can knock down!", w, 360);
+    ctx.drawImage(insEnd, w/2-40, 400, 450, 200);
+    ctx.font='900 50px Comic Sans MS';
+    ctx.fillStyle = "black";
+    ctx.fillText("Go!", w, 520);
+    insEnd.path = new Path2D();
+    insEnd.path.rect(w/2-40, 465, 450, 200);
+    canvas.addEventListener("click", InsEnd);
+}
+
+function Game() {
+    ctx.drawImage(bowlLane, 0, 0, 715, 750);
+    ctx.drawImage(ball, 330, 640, 85, 85);
 }
 
 
@@ -45,7 +100,15 @@ function playGame() {
 
     if (splashSc) {
         Splash();
-    }    
+    }
+    
+    if (insSc) {
+        Ins();
+    }  
+    
+    if (gameSc) {
+        Game();
+    }   
 
 }
 
